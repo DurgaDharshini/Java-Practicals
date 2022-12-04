2cd0827d9b094a1b1578ee585ba91d3a65920f8f
DurgaDharshini <119677808+DurgaDharshini@users.noreply.github.com>
2022-12-04 12:12:16 +0530

    Add files via upload

---
+++ data types/Hello_World.java
@@ -0,0 +1,40 @@
+package basic;
+
+public class Hello_World {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//boolean example
+        boolean IoT=true;
+        System.out.println(IoT);
+        //byte example
+         byte range;
+         range=127;
+         System.out.println(range);
+         //short example
+         short range1;
+         range1=-127;
+         System.out.println(range1);
+         //int ex
+         int range2=100000000;
+         System.out.println(range2);
+         //long ex
+         long range3=10000000000L;
+         System.out.println(range3);
+         //double ex
+         double number=99.9;
+         System.out.println(number);
+         //float ex
+         float number1=99.9f;
+         System.out.println(number1);	
+         //char example
+         char letter='\u0041';
+         System.out.println(letter);
+         char letter2=65;
+         System.out.println(letter2);
+         char letter3='A';
+         System.out.println(letter3);
+        
+	}
+
+}
